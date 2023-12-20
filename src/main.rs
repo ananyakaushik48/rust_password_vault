544bdd823b8d3e39708e85c2c9fcb020f33034ff
ananya <ananyakaushik48@gmail.com>
2023-12-21 04:03:20 +0530

    initialised: db.rs, handlers.rs, main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
 // The struct is always made first
+use chrono::prelude::*;
+pub struct User {
+    pub id: String,
+    pub user_id: String,
+    pub email_str: String,
+    pub mobile_str: String,
+    pub pincode_hash: String,
+    pub added_at: DateTime<Utc>,
+    pub description: String,
+    pub tags: Vec<String>,
+}
